3e818d819414d1f002362f73bf46e84f70eec4d4
Rheriel <rheriel@hotmail.co.jp>
2016-08-01 20:35:51 -0500

    Completed CRUD Actions
    Completed Update, Create and Delete actions.

--- app/controllers/PostController.scala
+++ app/controllers/PostController.scala
@@ -19,18 +19,51 @@ class PostController @Inject() (val reactiveMongoApi: ReactiveMongoApi) extends 
     implicit request => postRepo.find().map(posts => Ok(Json.toJson(posts)))
   }
 
-	def create = TODO
+	def create = Action.async(BodyParsers.parse.json) { 
+    implicit request =>
+      // TODO Define and clean this structure.
+      val id = (request.body \ Id).as[String]
+      val name = (request.body \ Name).as[String]
+      val description = (request.body \ Description).as[String]
+      val owner = (request.body \ Owner).as[String]
+      val fullAddress = (request.body \ FullAddress).as[String]
+      val simplifiedAddress = (request.body \ SimplifiedAddress).as[String]
+      val telephone = (request.body \ Telephone).as[String]
+      val latLong = (request.body \ LatLong).as[String]
+      
+      postRepo.save(BSONDocument(Id -> id, Name -> name, Description -> description, 
+          Owner -> owner, FullAddress -> fullAddress, SimplifiedAddress -> simplifiedAddress,
+          Telephone -> telephone, LatLong -> latLong)).map(result => Created)
+  }
 
 	def read(id: String) = Action.async { 
     implicit request => postRepo.select(BSONDocument(Id -> BSONObjectID(id))).map(post => Ok(Json.toJson(post))) }
 
-	def update(id: String) = TODO
+	def update(id: String) = Action.async(BodyParsers.parse.json) {
+    implicit request =>
+      // TODO Clean this as well.
+      val id = (request.body \ Id).as[String]
+      val name = (request.body \ Name).as[String]
+      val description = (request.body \ Description).as[String]
+      val owner = (request.body \ Owner).as[String]
+      val fullAddress = (request.body \ FullAddress).as[String]
+      val simplifiedAddress = (request.body \ SimplifiedAddress).as[String]
+      val telephone = (request.body \ Telephone).as[String]
+      val latLong = (request.body \ LatLong).as[String]
+      
+      postRepo.update(BSONDocument(Id -> BSONObjectID(id)),
+          BSONDocument("$set" -> BSONDocument(Id -> id, Name -> name, Description -> description, 
+          Owner -> owner, FullAddress -> fullAddress, SimplifiedAddress -> simplifiedAddress,
+          Telephone -> telephone, LatLong -> latLong))).map(result => Accepted)
+  }
 
-	def delete(id: String) = TODO
+	def delete(id: String) = Action.async {
+    postRepo.remove(BSONDocument(Id -> BSONObjectID(id))).map(result => Accepted) 
+  }
 }
 
 object PostFields {
-  val Id = "_id"
+  val Id = "id"
   val Name = "name"
   val Description = "description"
   val Owner = "owner"
